21ba210d223895e5f55d82928051c0caff769e30
Leila <nb.leila10@gmail.com>
2019-10-12 11:47:37 -0500

    Implement ex1 - problem if two same weights in the weights array

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,10 +9,23 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(len(weights)):
+        weight = weights[i]
+        hash_table_insert(ht, weight, i)
+    
+    for weight in weights:
+        print(hash_table_retrieve(ht, weight))
+    print("****")
 
+    for weight in weights:
+        diff = limit - weight
+        value = hash_table_retrieve(ht, diff)
+        if value is not None:
+            index = hash_table_retrieve(ht, weight)
+            if index > value:
+                return [index, value]
+            else:
+                return [value, index]
     return None
 
 
@@ -21,3 +34,11 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+# weights_2 = [4, 6, 3, 2]
+# answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
+weights_2 = [4, 4]
+answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
+# weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+# answer_4 = get_indices_of_item_weights(weights_4, 9, 7)
+print(answer_2)
